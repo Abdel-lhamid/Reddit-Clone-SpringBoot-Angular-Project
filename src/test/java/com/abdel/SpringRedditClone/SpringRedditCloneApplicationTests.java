aca685cb63b332624f2f472762bcc7e7dce3f688
Abdel-lhamid <abdelhamidbusiness@gmail.com>
2023-12-09 16:48:07 +0100

    project set up

---
+++ src/test/java/com/abdel/SpringRedditClone/SpringRedditCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abdel.SpringRedditClone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedditCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
